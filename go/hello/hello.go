96b0f22cf8f20176963789b31a3c26c91bf0a962
Tom Xu <170015@qq.com>
2020-01-30 16:36:20 +0800

    v130

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -2,5 +2,9 @@ package hello
 
 // Hello hello test
 func Hello() string {
-	return "Hello, world. gmt2 V120"
+	return "Hello, world. gmt2 V130"
+}
+
+func HelloNew() string {
+	return "Hello, new world. gmt2 v130"
 }
